5fb205ee988ebafd9a9ecd48da2dff746617644d
danielcampos <ddregalo@gmail.com>
2018-05-07 18:02:27 +0100

    Add Bootstrap + navbar and update tests for index route

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,6 +8,13 @@ from flask import(
 def test_app_index_status_ok(client):
     assert client.get(url_for('index')).status_code == 200
 
-def test_app_index_page_title(client):
+def test_app_index_page_navbar(client):
     response = client.get('/')
     assert b'CHATTER' in response.data
+    assert b'HOME' in response.data
+    assert b'ABOUT' in response.data
+    assert b'ARTICLES' in response.data
+
+def test_app_index_page_title(client):
+    response = client.get('/')
+    assert b'WELCOME TO CHATTER' in response.data
